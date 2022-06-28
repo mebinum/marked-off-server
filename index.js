0d87a3f1e745b83e6483fe7788f5441ab81ec938
Glitch (glitch-hello-node) <none>
2022-06-28 14:29:12 +0000

    👫🐊 Checkpoint ./package.json:32140581/2658 ./index.js:32140581/14360

--- index.js
+++ index.js
@@ -82,12 +82,20 @@ async function getIssuesFromNotionDatabase() {
     cursor = next_cursor
   }
   console.log(`${pages.length} issues successfully fetched.`)
-  return pages.map(page => {
-    return {
-      pageId: page.id,
-      issueNumber: page.properties["Issue Number"].number,
-    }
-  })
+  return await Promise.all(
+    pages.map(async page => {
+      const issueNumberPropertyId = page.properties["Issue Number"].id
+      const propertyResult = await notion.pages.properties.retrieve({
+        page_id: page.id,
+        property_id: issueNumberPropertyId,
+      })
+      console.log("*** propertyResult", propertyResult)
+      return {
+        pageId: page.id,
+        issueNumber: propertyResult.number,
+      }
+    })
+  )
 }
 
 /**
