6ff230dfba27810219cf914aec0d43e8bb4afcc1
Glitch (glitch-hello-node) <none>
2022-09-21 09:23:10 +0000

    ⚡️🍙 Checkpoint ./index.js:52550/1210 ./package.json:52550/495

--- index.js
+++ index.js
@@ -2,21 +2,23 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+const koa = require('koa');
+const app = koa();
+const PORT = process.env.PORT || 3000;
+
+
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+// Simple in-memory store for now
+var dreams = [
+  "Find and count some sheep",
+  "Climb a really tall mountain",
+  "Wash the dishes"
+];
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
-
-app.get("/dreams", function (request, response) {
+app.get("/callback", function (request, response) {
   response.send(dreams);
 });
 
@@ -26,14 +28,9 @@ app.post("/dreams", function (request, response) {
   response.sendStatus(200);
 });
 
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+
+
+const server = app.listen(PORT, () => {
+console.log(`Koa is listening to http://localhost:${server.address().port}`);
 });
