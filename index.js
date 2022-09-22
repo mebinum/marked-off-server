53ba83aafeeec48534eecbb91e70e83bd2b4c8e6
Glitch (glitch-hello-node) <none>
2022-09-22 05:54:54 +0000

    🏔🍜 Checkpoint ./index.js:52550/7149

--- index.js
+++ index.js
@@ -1,82 +1,106 @@
 // server.js
 // where your node app starts
 
-
-const express = require('express');
-const { Client } = require("@notionhq/client");
-const dotenv = require("dotenv");
-const assets = require('./assets');
-const { NotionPageToPdf } = require('./notionPageToPdf');
+const express = require("express")
+const { Client } = require("@notionhq/client")
+const dotenv = require("dotenv")
+const assets = require("./assets")
+const { NotionPageToPdf } = require("./notionPageToPdf")
 
 dotenv.config()
 
-const hellosign = require('hellosign-sdk')({ key: process.env.HELLOSIGN_KEY });
+const hellosign = require("hellosign-sdk")({ key: process.env.HELLOSIGN_KEY })
 
 // init project
-var app = express();
+var app = express()
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"))
 
-app.use("/assets", assets);
+app.use("/assets", assets)
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 const notion = new Client({ auth: process.env.NOTION_KEY })
 
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// we've started you off with Express, 
+// we've started you off with Express,
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // Simple in-memory store for now
 var dreams = [
   "Find and count some sheep",
   "Climb a really tall mountain",
-  "Wash the dishes"
-];
-
+  "Wash the dishes",
+]
 
 app.get("/", function (request, response) {
-  response.json({"Hello": "world"});
-});
-
+  response.json({ Hello: "world" })
+})
 
 app.post("/hellosign-events", function (request, response) {
-  console.log("callback", request);
-  
+  console.log("callback data", request.body)
+  //   var postData = JSON.parse(request.body);
+  //console.log("postData data", postData);
+
   //response.send(dreams);
-  response.set('content-Type', 'text/plain');
-  response.status(200).send('Hello API Event Received');
-});
+  response.set("content-Type", "text/plain")
+  response.status(200).send("Hello API Event Received")
+})
 
 app.get("/markoff/:pageId", (request, response) => {
-  
   //get pageid
-  const pageId = request.params.pageId;
+  const pageId = request.params.pageId
   //generate pdf
-  const pdfUrl = NotionPageToPdf.toPdf(pageId);
+  const pdfUrl = NotionPageToPdf.toPdf(pageId)
   //send pdf to hellosign api
-  
+
   const opts = {
-    test_mode: 1,
-    clientId: process.env.HELLOSIGN_CLIENTID,
-    type: 'request_signature',
-    subject: 'The NDA we talked about',
-    requester_email_address: 'alice@example.com',
-    files: ['NDA.pdf']
-};
-  
-});
+    title: "NDA with Acme Co.",
+    subject: "The NDA we talked about",
+    message:
+      "Please sign this NDA and then we can discuss more. Let me know if you\nhave any questions.",
+    signers: [
+      {
+        email_address: "oyem@sheda.ltd",
+        name: "Oyem",
+        order: 0,
+      },
+      {
+        email_address: "mike@sheda.ltd",
+        name: "Jill",
+        order: 1,
+      },
+    ],
+    //cc_email_addresses: ["lawyer@hellosign.com", "lawyer@example.com"],
+    file_url: [
+      pdfUrl,
+    ],
+    // metadata: {
+    //   custom_id: 1234,
+    //   custom_text: "NDA #9",
+    // },
+    signing_options: {
+      draw: true,
+      type: true,
+      upload: true,
+      phone: false,
+      default_type: "draw",
+    },
+    field_options: {
+      date_format: "DD - MM - YYYY",
+    },
+    test_mode: true,
+  };
+})
 
 // could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
 app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
-
+  dreams.push(request.query.dream)
+  response.sendStatus(200)
+})
 
 const server = app.listen(PORT, () => {
-    console.log(`Express is listening to http://localhost:${PORT}`);
-});
+  console.log(`Express is listening to http://localhost:${PORT}`)
+})
