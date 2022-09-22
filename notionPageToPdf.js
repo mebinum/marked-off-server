65c2edd1655fffc1fcbaa90f19426a3ad3b80dba
Glitch (glitch-hello-node) <none>
2022-09-22 04:58:21 +0000

    👨‍❤️‍👨🍝 Checkpoint ./.glitch-assets:52550/353 ./notionPageToPdf.js:52550/314

--- notionPageToPdf.js
+++ notionPageToPdf.js
@@ -1,7 +1,16 @@
+const fs = require('fs/promises');
+
 class NotionPageToPdf {
   
-  static toPdf(pageId) {
-    
+  static async toPdf(pageId) {
+     try {
+       console.log("pageId",pageId)
+      const data = await fs.readFile('./assets/contract.pdf', { encoding: 'utf8' });
+      console.log(data);
+       return data;
+    } catch (err) {
+      console.log(err);
+    }
   }
 }
 
