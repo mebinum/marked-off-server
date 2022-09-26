a08c7e528d38afe09bda1c1b2d4abe8ce31d913d
Glitch (glitch-hello-node) <none>
2022-09-26 11:36:50 +0000

    🎱⛪️ Checkpoint ./package.json:52550/1038 ./notionPageToPdf.js:52550/429

--- notionPageToPdf.js
+++ notionPageToPdf.js
@@ -1,13 +1,21 @@
 const findAssetInfo = require('./findAssetInfo.js');
-
 const { notionClient } = require("./notionClient");
-
+const { Notion2Html } = require("@jeufore/notion-2-html");
+const dotenv = require("dotenv");
+dotenv.config();
 
 class NotionPageToPdf {
   
   static async toPdf(pageId) {
      try {
-       console.log("pageId",pageId)
+       console.log("pageId",pageId);
+       console.log("Notion2Html",Notion2Html);
+       
+       const notion = new Notion2Html(process.env.NOTION_KEY);
+       
+       const html =  await notion.convert(pageId);
+       console.log("notion html", html);
+       
        //get notion page
        const response = await notionClient.pages.retrieve({ page_id: pageId });
        console.log("notion page", response);
