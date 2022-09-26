92d59082db590b3e16bfb0d7f99fe1d6a50bf995
Glitch (glitch-hello-node) <none>
2022-09-26 09:30:53 +0000

    👛💋 Checkpoint ./middleware.js:52550/192 ./index.js:52550/10688

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const { Client } = require("@notionhq/client")
 const dotenv = require("dotenv")
 const assets = require("./assets")
 const { NotionPageToPdf } = require("./notionPageToPdf")
-const { notionClient } = require("./notionClient");
+const { notionClient } = require("./notionClient")
 const {
   logErrors,
   clientErrorHandler,
   errorHandler,
-  allowCors
-} = require("./middleware");
+  allowCors,
+} = require("./middleware")
 
 dotenv.config()
 
@@ -29,13 +29,10 @@ app.use("/assets", assets)
 const PORT = process.env.PORT || 3000
 const notion = new Client({ auth: process.env.NOTION_KEY })
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
 //allow cors
-app.use(allowCors);
-app.use(express.json());
-app.use(logErrors);
-app.use(clientErrorHandler);
-app.use(errorHandler);
+app.use(allowCors)
+app.use(express.json())
 
 // we've started you off with Express,
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
@@ -66,67 +63,71 @@ app.post("/hellosign-events", function (request, response) {
 })
 
 app.post("/markoff/:pageId", async (request, response, next) => {
-  //get pageid
-  const pageId = request.params.pageId
-  const requestData = request.body;
-  console.log("body", requestData);
-  //generate pdf
-  const pdfUrl = await NotionPageToPdf.toPdf(pageId).catch(next)
-  //send pdf to hellosign api
+  try {
+    //get pageid
+    const pageId = request.params.pageId
+    const requestData = request.body
+    console.log("body", requestData)
+    //generate pdf
+    const pdfUrl = await NotionPageToPdf.toPdf(pageId);
+    //send pdf to hellosign api
 
-  const signer1 = {
-    email_address: "oyem@sheda.ltd",
-    name: "Oyem",
-    order: 0,
-  }
+    const signer1 = {
+      email_address: "oyem@sheda.ltd",
+      name: "Oyem",
+      order: 0,
+    }
 
-  const signer2 = {
-    email_address: "mike@sheda.ltd",
-    name: "Mike",
-    order: 1,
-  }
+    const signer2 = {
+      email_address: "mike@sheda.ltd",
+      name: "Mike",
+      order: 1,
+    }
 
-  const signingOptions = {
-    draw: true,
-    type: true,
-    upload: true,
-    phone: false,
-    default_type: "draw",
-  }
+    const signingOptions = {
+      draw: true,
+      type: true,
+      upload: true,
+      phone: false,
+      default_type: "draw",
+    }
 
-  const fieldOptions = {
-    date_format: "DD - MM - YYYY",
-  }
-  
-  // signing_redirect_url: "URL of the notion page"
-  
-  const data = {
-    title: "NDA with Acme Co.",
-    subject: "The NDA we talked about",
-    message:
-      "Please sign this NDA and then we can discuss more. Let me know if you have any questions.",
-    signers: [signer1, signer2],
-    //ccEmailAddresses: ["lawyer@hellosign.com", "lawyer@example.com"],
-    // signing_redirect_url: 'http://bondstreet.co.uk',
-    // requesting_redirect_url: 'http://met.police.uk',
-    file_url: [pdfUrl],
-    // metadata: {
-    //   custom_id: 1234,
-    //   custom_text: "NDA #9",
-    // },
-    signing_options: signingOptions,
-    fieldOptions: fieldOptions,
-    test_mode: 0,
-  }
+    const fieldOptions = {
+      date_format: "DD - MM - YYYY",
+    }
 
-  const result = await hellosign.signatureRequest.send(data);
-  
-  const responseMessage = {
-    status: 200,
-    message: `Successfully Marked Off Notion page ${pageId}`
+    // signing_redirect_url: "URL of the notion page"
+
+    const data = {
+      title: "NDA with Acme Co.",
+      subject: "The NDA we talked about",
+      message:
+        "Please sign this NDA and then we can discuss more. Let me know if you have any questions.",
+      signers: [signer1, signer2],
+      //ccEmailAddresses: ["lawyer@hellosign.com", "lawyer@example.com"],
+      // signing_redirect_url: 'http://bondstreet.co.uk',
+      // requesting_redirect_url: 'http://met.police.uk',
+      file_url: [pdfUrl],
+      // metadata: {
+      //   custom_id: 1234,
+      //   custom_text: "NDA #9",
+      // },
+      signing_options: signingOptions,
+      fieldOptions: fieldOptions,
+      test_mode: 0,
+    }
+
+    const result = await hellosign.signatureRequest.send(data)
+
+    const responseMessage = {
+      status: 200,
+      message: `Successfully Marked Off Notion page ${pageId}`,
+    }
+
+    console.log(result.body)
+  } catch (err) {
+    next(err)
   }
-  
-  console.log(result.body);
 })
 
 // could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
@@ -134,6 +135,10 @@ app.post("/dreams", function (request, response) {
   dreams.push(request.query.dream)
   response.sendStatus(200)
 })
+
+app.use(logErrors)
+app.use(clientErrorHandler)
+app.use(errorHandler)
 
 const server = app.listen(PORT, () => {
   console.log(`Express is listening to http://localhost:${PORT}`)
--- middleware.js
+++ middleware.js
@@ -1,19 +1,20 @@
 function logErrors (err, req, res, next) {
-  console.error(err.stack)
+  console.error("====logging error====");
+  console.error(err.stack);
   next(err)
 }
 
 function clientErrorHandler (err, req, res, next) {
   if (req.xhr) {
-    res.status(500).send({ error: err })
+    res.status(500).json({ error: err })
   } else {
     next(err)
   }
 }
 
 function errorHandler (err, req, res, next) {
-  res.status(500)
-  res.render('error', { error: err })
+  res.status(500);
+  res.json({ error: err });
 }
 
 function allowCors (req, res, next) {
