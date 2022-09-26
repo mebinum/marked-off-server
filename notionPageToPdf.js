972050988de02307fe9ee8fe5dd16620d0c1930b
Glitch (glitch-hello-node) <none>
2022-09-26 09:54:50 +0000

    🗺👘 Checkpoint ./notionPageToPdf.js:52550/288

--- notionPageToPdf.js
+++ notionPageToPdf.js
@@ -12,8 +12,14 @@ class NotionPageToPdf {
        const response = await notionClient.pages.retrieve({ page_id: pageId });
        console.log("notion page", response);
        
+       const pageBlocks = await notionClient.blocks.children.list({
+          block_id: pageId,
+       });
+       
+       console.log("notion page blocks", pageBlocks.results.length);
+       
       const assetInfo = findAssetInfo('contract.pdf');
-      console.log("assetInfo", assetInfo);
+      //console.log("assetInfo", assetInfo);
        return assetInfo.url;
     } catch (err) {
       console.log(err);
